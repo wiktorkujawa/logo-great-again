ea81821cc0dce6608d1985534e1c4e00cc021a04
wiktor <wiktorkujawa1993@gmail.com>
2021-04-30 02:14:13 +0200

    Created routers, and provided store

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, { FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useStore } from './store';
+
+import { Switch, Route } from 'react-router-dom';
+import { NotFoundPage } from './components/NotFoundPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App">      
+      <Switch>
+        <Route component={NotFoundPage} />
+      </Switch>
+     </div>
   );
 }
+
+
 
 export default App;
